107e9f35ba4a1e5a99ac8bd037a9341f562029cd
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-07-02 18:57:17 +0700

    newNode leftchild dan rightchild menunjuk ke null

--- binarysearchtree_103/binarysearchtree_103.cpp
+++ binarysearchtree_103/binarysearchtree_103.cpp
@@ -32,6 +32,8 @@ public:
 	{
 		Node* newNode = new Node(element, NULL, NULL); // Alocated memory for the new node
 		newNode->info = element; // Assign value to data field of the new node
+		newNode->leftchild = NULL; // Make the left child of the new node point to NULL
+		newNode->rightchild = NULL; // Make the riht child of the new node point to NULL
 
 	}
 };
